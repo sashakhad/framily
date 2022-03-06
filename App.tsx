8d5435895482a8d710c4f7af48a42510a9940bf6
Sasha <sashabayan@gmail.com>
2022-03-06 11:18:55 -0800

    access contacts list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+  const [contacts, setContacts] = useState<Contacts.Contact[]>([])
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [Contacts.Fields.Emails],
+        });
+
+        if (data.length > 0) {
+          setContacts(data)
+          const contact = data[0];
+          console.log(contact);
+        }
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Shmoosh</Text>
-      <StatusBar style="auto" />
+      {contacts.map(({firstName, lastName}) => (
+        <>
+          <Text> {firstName} </Text>
+          <Text> {lastName} </Text>
+        </>
+      ))}
+      <Text>Contacts Module Example</Text>
     </View>
   );
 }
